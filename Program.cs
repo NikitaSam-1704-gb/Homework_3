cd292084717e5f82d7c1cc72796bb1382edfb86f
Никита <samnikita141204@yandex.ru>
2022-11-28 17:02:22 +0400

    Задача 5 урок 3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿// Задать N и вывести # до N елочкой 
+//пример N = 3;     # 
+//                 ###
+//N = 5   # 
+//       ###
+//      #####
+//N = 7      # 
+//          ###
+//         #####
+//        #######
+
+//Двумерный цикл 
+//Если у нас не должно быть # , то пишем пробел
+
+using static System.Console ;
+Clear ();
+ Write(" Введите нечетное число   ") ;
+ int n=Convert.ToInt16(ReadLine());
+ int leftBorder=n/2+1;
+ int reghtBorder=n/2+1;
+
+ for(int i=0; i<(n+1)/2; i++ )
+ {
+    for ( int j=0; j<=n; j++)
+    {
+        if(j>=leftBorder && j<=reghtBorder)
+            Write("#");
+        else Write(" ");    
+    }
+    WriteLine();
+    leftBorder=leftBorder-1 ;
+    reghtBorder=reghtBorder+1;
+ }
