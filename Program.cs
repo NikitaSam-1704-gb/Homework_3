dd5d10919e9fceb952b971fcb35b2a4af0a23251
Никита <samnikita141204@yandex.ru>
2022-11-28 23:04:45 +0400

    Задача 23 урок 3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+using static System.Console ;
+Clear() ;
+
+Write(" Введите число N ") ;
+int n=Convert.ToInt16(ReadLine()) ;
+int summ = 0;
+Write(" -> ") ;
+for( int i=1; i<=n; i++)
+{
+    summ=Convert.ToInt32(Math.Pow(i, 3));
+    Write($"  {summ} ") ;
+    summ=0 ;
+}
+WriteLine();
